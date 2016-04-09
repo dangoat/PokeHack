0d49d5cf9cee11c3fc382fb9108fb25d30dd494e
Alex Newgent <newgent@terpmail.umd.edu>
2016-04-09 01:31:49 -0400

    Update Pokemon.cs

--- PokeHack/Pokemon.cs
+++ PokeHack/Pokemon.cs
@@ -1,12 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
+using PokeAPI;
+
+// [...]
+
 
 namespace PokeHack
 {
-    class Pokemon
-    {
-    }
+	public class Pokemon	//Put everything in here
+	{
+		private PokemonSpecies p;
+		private Move[] MoveSet = new Move[4];
+		private int Level;
+		private int HealthMax;
+		private int HealthCurr;
+		private int Attack;
+		private int Defense;
+		private int SpecialAttack;
+		private int SpecialDefense;
+		private int Speed;
+		private int Accuracy = 100;
+		private int Evasiveness = 100;
+		private int Happiness;
+		private int Weight;
+		private string Type;
+
+		public Pokemon (int PokemonID, int level)
+		{
+			p = await DataFetcher.GetApiObject<PokemonSpecies>(PokemonID);
+			Level = level;
+			HealthMax = p.
+			//p = await DataFetcher.GetNamedApiObject<PokemonSpecies>("lucario");
+			float cRate = p.CaptureRate;
+			Attack = p.Attack;
+			Defense = p.Defense;
+			SpecialAttack = p.SpAtk;
+			SpecialDefense = p.SpDef;
+			Speed = p.Speed;
+			Happiness = p.Happiness;
+			Weight = p.Weight;
+			Type = p.Types["name"];
+			//Type = p.Types[Name];
+
+		}
+	}
 }
