3531c04d5077d4fd1a8312d732e446323983139a
dangoat <danevans@terpmail.umd.edu>
2016-04-09 03:04:14 -0400

    Update Types.cs

--- PokeHack/Types.cs
+++ PokeHack/Types.cs
@@ -1,4 +1,4 @@
-namespace PokeHack
+namespace Application
 {
     public enum Type { Normal, Fire, Water, Electric, Grass, Ice, Fighting, Poison, Ground, Flying, Psychic, Bug, Rock, Ghost, Dragon, Dark, Steel, Fairy, None };
 
